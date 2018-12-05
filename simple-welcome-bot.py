bf7f29926b7e3592f9f368387ee4c503ed1c307a
NikNikYT <45605688+NikNikYT@users.noreply.github.com>
2018-12-05 19:21:38 +0100

    Update simple-welcome-bot.py

--- simple-welcome-bot.py
+++ simple-welcome-bot.py
@@ -16,7 +16,7 @@ Clientdiscord = discord.Client()
 @client.event
 async def on_member_join(member):
     print('Recognised that a member called ' + member.name + ' joined')
-    await client.send_message(member, 'hi and welcome to arrow music show and here are the roles you may not talk unless you have been promoted and given permission to do it To become a fan you must agree by joining me in a voice call when you do that if I think you are worthy you will become a fan after you become a fan you can join in activities respond to my talking subscribe and that will rank you up faster by giving you points and no talking about love on the channel thanks')
+    await client.send_message(member, 'hi and welcome to arrow music show and here are the roles. you may not talk unless you have been promoted and given permission to do it, To become a fan you must agree by joining me in a voice call when you do that if I think you are worthy you will become a fan, after you become a fan you can join in activities respond to my talking subscribe and that will rank you up faster by giving you points and no talking about love on the channel thanks')
     print('Sent message to ' + member.name)
 async def on_ready():
     await client.change_presence(game=Game(name='you live', type = 3))
