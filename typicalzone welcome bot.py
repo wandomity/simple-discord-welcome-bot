f3315e42d5b5a30d0b4d144fbb25e1ba120f776f
NikNikYT <45605688+NikNikYT@users.noreply.github.com>
2018-12-05 19:01:59 +0100

    Add files via upload

---
+++ typicalzone welcome bot.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+import os
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_member_join(member):
+    print('Recognised that a member called ' + member.name + ' joined')
+    await client.send_message(member, 'hi and welcome to arrow music show and here are the roles you may not talk unless you have been promoted and given permission to do it To become a fan you must agree by joining me in a voice call when you do that if I think you are worthy you will become a fan after you become a fan you can join in activities respond to my talking subscribe and that will rank you up faster by giving you points and no talking about love on the channel thanks')
+    print('Sent message to ' + member.name)
+async def on_ready():
+    await client.change_presence(game=Game(name='you live', type = 3))
+    print('Ready, Freddy') 
+
+
+@client.event
+async def on_message(message):
+client.run(os.getenv('TOKEN'))
